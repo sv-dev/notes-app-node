4dc405b078cf5e3932c6b98aa911beeb6d310b49
user <gertin7@gmail.com>
2019-08-09 00:12:09 +0300

    Tasks have patch - UPDATE

--- src/index.js
+++ src/index.js
@@ -47,6 +47,34 @@ app.get('/users/:id', async (req, res) => {
   // console.log(req.params)
 })
 
+app.patch('users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+
+  const isValidOperation = updates.every(update =>
+    allowUpdates.includes(update)
+  )
+  if (isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
@@ -82,10 +110,12 @@ app.get('/tasks/:id', async (req, res) => {
   }
 })
 
-app.patch('users/:id', async (req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ['name', 'email', 'password', 'age']
 
+
+app.patch('tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  
   const isValidOperation = updates.every(update =>
     allowUpdates.includes(update)
   )
@@ -94,7 +124,7 @@ app.patch('users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
     })
