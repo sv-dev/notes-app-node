275252c1ecfc9d41c2d0f5489e6557e4dab76ade
user <gertin7@gmail.com>
2019-08-14 02:20:21 +0300

    Pagination and Sorting data

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,12 +20,20 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
-// GET /tasks/?completed=true
+// GET /tasks?completed=true
+// GET /tasks?limit=10$skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
   }
 
   try {
@@ -33,6 +41,11 @@ router.get('/tasks', auth, async (req, res) => {
       .populate({
         path: 'tasks',
         match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
       })
       .execPopulate();
     res.send(req.user.tasks);
