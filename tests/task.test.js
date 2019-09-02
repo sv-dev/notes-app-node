11b3f89bea1fd5e6175431b879323ddf30d4b4d0
user <gertin7@gmail.com>
2019-09-02 17:51:39 +0300

    Testing with fixtures

---
+++ tests/task.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../src/app');
+const Task = require('../src/models/task');
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
+
+beforeEach(setupDatabase);
+
+
